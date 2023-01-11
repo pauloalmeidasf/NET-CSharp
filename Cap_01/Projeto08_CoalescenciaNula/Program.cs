4b53f3306185aacb97bf46054fcb563267425eae
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-11 19:30:59 -0300

    Commit Projeto08_CoalescenciaNula do Capítulo 01

---
+++ Cap_01/Projeto08_CoalescenciaNula/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto08_CoalescenciaNula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * O operador de COALESCENCIA NULA retornará o valor do operando da ESQUERDA se não for NULL
+             * 
+             * Caso contrário, ele retornará o valor da DIREITA
+             * 
+             */
+
+            string nomeCurso = null;
+
+            string avaliacao = nomeCurso ?? "Curso não informado.";
+
+            Console.WriteLine($"Nome do Curso: {avaliacao}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
