c3101809b403296dc3afb87d0d269c9711c609ed
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-12 20:23:33 -0300

    Commit Projeto03_While do Capítulo 02

---
+++ Cap_02/Projeto03_While/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Quando não é sabido quantas vezes um determinado bloco de instruções deverá ser executado, utiliza-se o loop while
+ * 
+ */
+
+int num = 1000;
+
+while(num <= 5000)
+{
+
+    if(num == 3000)
+    {
+
+        num += 1000;
+
+        //retorna para a verificação da expressão, ou seja, retorna para while(expressão)
+        continue;
+
+    }
+
+    if(num == 5000)
+    {
+
+        //finaliza o loop
+        break;
+
+    }
+
+    Console.WriteLine($"Número {num}");
+
+    num += 1000; //num = num + 1000
+
+}
+
+Console.ReadKey();
