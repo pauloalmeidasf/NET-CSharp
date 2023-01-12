484b6575a1863ff746225639b0b422b3ecbf7ee7
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-11 22:06:14 -0300

    Commit Projeto13_ConcatenacaoStrings do Capítulo 01

---
+++ Cap_01/Projeto13_ConcatenacaoStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto13_ConcatenacaoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome = "Julio ";
+            string sobrenome = "Cesar";
+
+            string nomeCompleto = string.Concat(nome, sobrenome);
+
+            Console.WriteLine($"Nome completo: {nomeCompleto}");
+
+            string x = "50";
+            string y = "60";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Valor de x: {x}");
+            Console.WriteLine($"Valor de y: {y}");
+
+            Console.WriteLine($"Valor de x + y = {x + y}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Cap_01/Projeto13_ConcatenacaoStrings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Projeto13_ConcatenacaoStrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Projeto13_ConcatenacaoStrings")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71f3a9df-020b-4b60-9486-cff1de76161f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
