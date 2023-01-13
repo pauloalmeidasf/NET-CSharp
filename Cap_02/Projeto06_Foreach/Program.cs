3cea65f89d1b2072214aeb2f67546a533b60a3c6
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-12 21:43:19 -0300

    Commit Projeto06_Foreach do Capítulo 02

---
+++ Cap_02/Projeto06_Foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Foreach
+ * 
+ * Este loop é utilizado para percorrer coleções
+ * Ao ser executado, seu iterador percorrerá todos os elementos da coleção, permitindo o acesso
+ * às suas propriedades e métodos
+ * 
+ */
+
+string[] lista = { "Brasil", "Alemanha", "México", "Chile", "Holanda", "Polônia", "Argentina", "França" };
+
+foreach(var pais in lista)
+{
+
+    if (pais.Contains("m"))
+    {
+
+        Console.WriteLine($"Nome do país: {pais}");
+
+    }
+
+}
+
+Console.ReadKey();
