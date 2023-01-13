5151fe44927fe5902ac1b60314631d4c53f4b050
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-13 09:36:58 -0300

    Commit Exemplo do tipo Enumeração

---
+++ Exemplo-Enum/Exemplo/Estados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula
+{
+
+    enum Estados
+    {
+
+        SP, MG, RS, SC, PE, BA, GO
+
+    }
+
+}
---
+++ Exemplo-Enum/Exemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Aula;
+
+List<string> listaEstados = new();
+
+foreach (Estados e in Enum.GetValues(typeof(Estados)))
+{
+
+    //Armazena a enumeração em uma lista
+    listaEstados.Add(e.ToString());
+ 
+}
+
+Console.ReadKey();
