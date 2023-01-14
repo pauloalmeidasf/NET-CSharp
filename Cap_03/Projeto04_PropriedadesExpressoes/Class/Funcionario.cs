beed0258e098ad56995f0e5456ed83ab489ebbb9
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-13 21:56:13 -0300

    Commit Projeto04_PropriedadesExpressoes Capítulo 03

---
+++ Cap_03/Projeto04_PropriedadesExpressoes/Class/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04_PropriedadesExpressoes.Class
+{
+    internal class Funcionario
+    {
+
+        /*
+         * Propriedades Expressões
+         * 
+         */
+
+        private string _nome;
+
+        public string Nome {
+
+            get => _nome;
+            set => _nome = value;
+
+        }
+
+        private decimal _salario;
+
+        public decimal Salario
+        {
+
+            get => _salario;
+            set => _salario = value;
+
+        }
+
+        public decimal Irpf
+        {
+
+            get => Salario * 0.10m;
+
+        }
+
+    }
+}
