55f4dd18efcbb2ccf2e84bd2e233c5300ddbf1a3
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-13 22:54:24 -0300

    Commit Projeto07_Enumeradores Capítulo 03

---
+++ Cap_03/Projeto07_Enumeradores/Enum/EstadosCivis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto07_Enumeradores.Enum
+{
+    /*
+     * Enum
+     * 
+     * É utilizado para declarar um CONJUNTO de CONSTANTES. Um enumerador não poder herdar ou ser herdado
+     * 
+     * O tipo padrão de um enumerador é um int
+     * 
+     */
+
+    internal enum EstadosCivis
+    {
+
+        Casado, Solteiro, Viúvo, Divorciado, Outro
+
+    }
+}
---
+++ Cap_03/Projeto07_Enumeradores/Program.cs
@@ -0,0 +1,30 @@
+﻿using Projeto07_Enumeradores.Enum;
+
+Console.WriteLine("Todos os estados civis");
+
+Console.WriteLine();
+
+//Loop para ler todos os estados civis
+foreach(var i in Enum.GetValues(typeof(EstadosCivis)))
+{
+
+    Console.WriteLine($"Estado Civil: {i}");
+
+}
+
+Console.WriteLine();
+
+Console.WriteLine(new string('-',50));
+
+Console.WriteLine();
+
+Console.WriteLine("Estado civil individual");
+
+Console.WriteLine();
+
+//Enum para string
+string casado = $"Estado civil: {EstadosCivis.Casado}";
+
+Console.WriteLine(casado);
+
+Console.ReadKey();
