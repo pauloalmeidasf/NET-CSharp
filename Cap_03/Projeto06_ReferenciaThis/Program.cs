49239b1b5bd2c1c9ac67d6087b82f498675440b0
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-13 22:31:52 -0300

    Commit Projeto06_ReferenciaThis Capítulo 03

---
+++ Cap_03/Projeto06_ReferenciaThis/Program.cs
@@ -0,0 +1,11 @@
+﻿using Projeto06_ReferenciaThis.Class;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.ObterDados("Lorran", 18, 'M');
+
+Console.WriteLine($"Nome: {pessoa.Nome}");
+Console.WriteLine($"Idade: {pessoa.Idade}");
+Console.WriteLine($"Gênero: {pessoa.Genero}");
+
+Console.ReadKey();
