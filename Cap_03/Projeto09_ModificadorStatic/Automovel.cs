746904c3570ff046ebdec5d745461543165e48fb
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-16 20:05:23 -0300

    Commit Projeto09_ModificadorStatic Capítulo 03

---
+++ Cap_03/Projeto09_ModificadorStatic/Automovel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto09_ModificadorStatic
+{
+    internal class Automovel
+    {
+
+        public static string Fabricante { get; set; }
+
+        public static string Modelo { get; set; }
+
+        public string ExibirDados()
+        {
+
+            return $"Fabricante: {Fabricante}\nModelo: {Modelo}";
+
+        }
+
+    }
+}
---
+++ Cap_03/Projeto09_ModificadorStatic/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Projeto09_ModificadorStatic;
+
+Automovel automovel = new Automovel();
+
+Automovel.Fabricante = "VolksWagen";
+Automovel.Modelo = "T-Cross";
+
+Automovel.Fabricante = "Chevrolet";
+Automovel.Modelo = "Onix";
+
+Console.WriteLine(automovel.ExibirDados());
+
+Console.ReadKey();
