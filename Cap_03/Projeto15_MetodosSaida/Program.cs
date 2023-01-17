962cdbe9464b045c385a01d1abe31d8521efd3c8
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-17 20:54:12 -0300

    Commit Projeto15_MetodosSaida Capítulo 03

---
+++ Cap_03/Projeto15_MetodosSaida/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Método de saída: out
+ * 
+ * O método atribui valor ao parâmetro
+ * 
+ */
+
+static void RetornaPI(out double valorPi)
+{
+
+    double pi = Math.PI; //3.1415...
+
+    valorPi = pi;
+
+}
+
+double constantePi;
+
+RetornaPI(out constantePi);
+
+Console.WriteLine($"Valor de pi: {constantePi}");
+
+Console.ReadKey();
