5f34438320d6b7e5b1b45f3ea8eedcc009bc6c68
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-16 22:30:24 -0300

    Commit Projeto12_MetodosEstaticos Capítulo 03

---
+++ Cap_03/Projeto12_MetodosEstaticos/Class/Metodos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto12_MetodosEstaticos.Class
+{
+
+    /*
+     * Métodos estáticos
+     * 
+     * Não faz uso da instância à qual pertence para ser utilizado
+     * 
+     */
+
+    internal class Metodos
+    {
+
+        public static string RetirarAcentos(string texto)
+        {
+
+            string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for(int i = 0; i < comAcentos.Length; i++)
+            {
+
+                texto = texto.Replace(comAcentos[i], semAcentos[i]);
+
+            }
+
+            return texto;
+
+        }
+        
+    }
+}
