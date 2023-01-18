082217c4f243eabf81664968c8ecbd524f7a05fa
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-17 21:33:58 -0300

    Commit Projeto16_ParametrosOpcionaisDefault Capítulo 03

---
+++ Cap_03/Projeto16_ParametrosOpcionaisDefault/Enum/Estados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto16_ParametrosOpcionaisDefault.Enum
+{
+    internal enum Estados
+    {
+
+        SP, MG, RJ, RS, SC, PR, PE, BA, MA
+
+    }
+}
