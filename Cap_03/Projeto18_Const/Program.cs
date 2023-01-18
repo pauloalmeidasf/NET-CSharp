40a1e7dc93315896e50a0daf9abc92e8e3c9ba2f
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-17 22:16:16 -0300

    Commit Projeto18_Const Capítulo 03

---
+++ Cap_03/Projeto18_Const/Program.cs
@@ -0,0 +1,9 @@
+﻿using Projeto18_Const.Class;
+
+string servidor = Constantes.SERVIDOR;
+
+int velocidadeLuz = Constantes.VELOCIDADE_LUZ;
+
+Console.WriteLine($"Endereço do Servidor: {servidor}");
+
+Console.WriteLine($"Velocidade da Luz: {velocidadeLuz}");
