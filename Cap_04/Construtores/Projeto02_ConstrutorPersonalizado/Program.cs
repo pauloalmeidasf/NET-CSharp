5899be7424a9cad17f296b7bc869a96fc98bab2d
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-18 20:43:38 -0300

    Commit Projeto03_ConstrutorSobrecarregado Capítulo 04

--- Cap_04/Construtores/Projeto02_ConstrutorPersonalizado/Program.cs
+++ Cap_04/Construtores/Projeto02_ConstrutorPersonalizado/Program.cs
@@ -1,9 +1,19 @@
 ﻿using Projeto02_ConstrutorPersonalizado.Class;
 
-//Colaborador colaborador = new Colaborador("Sofia","Médica",25000);
+//Objeto instanciado com o construtor personalizado
+Colaborador c1 = new Colaborador("Sofia","Médica",25000);
 
-Colaborador colaborador = new Colaborador();
+//Objeto instanciado com o construtor default (padrão)
+Colaborador c2 = new Colaborador();
 
-Console.WriteLine(colaborador.DadosColaborador());
+Console.WriteLine(c1.DadosColaborador());
+
+Console.WriteLine();
+
+Console.WriteLine(new string('-',60));
+
+Console.WriteLine();
+
+Console.WriteLine(c2.DadosColaborador());
 
 Console.ReadKey();
