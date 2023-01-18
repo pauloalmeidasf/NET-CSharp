751a22da148c9b66057af6ede42c3bce34b2c2a8
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-17 22:39:36 -0300

    Commit Projeto19_ClassesAnonimas Capítulo 03

---
+++ Cap_03/Projeto19_ClassesAnonimas/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Classes Anônimas
+ * 
+ * Uma classe anônima é uma classe que não tem nome
+ * 
+ * Utilizada em expressões de consultas
+ * 
+ * As classes anônimas só podem conter campos públicos e todos esses campos precisam ser inicializados,
+ * não podem ser estáticos e não pode conter nenhum método
+ * 
+ */
+
+//Objeto de uma classe anônima
+var objetoAnonimo = new { Nome = "Andrea", Idade = 25, Genero = 'F' };
+
+var outroObjetoAnonimo = new { Nome = "Paulo", Idade = 40, Genero = 'M'};
+
+//var outroObjetoAnonimo = new { Nome = "Paulo", Idade = 40, Genero = 'M' , Escolaridade = "Ensino Superior"};
+
+objetoAnonimo = outroObjetoAnonimo;
+
+Console.WriteLine($"Nome: {objetoAnonimo.Nome}\nIdade: {objetoAnonimo.Idade}\nGênero: {objetoAnonimo.Genero}");
+
+Console.ReadKey();
