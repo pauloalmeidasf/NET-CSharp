a9ff688bcb4613cb8b7b51a008fedac84f12d2da
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-18 19:50:21 -0300

    Commit Projeto02_ConstrutorPersonalizado Capítulo 04

---
+++ Cap_04/Construtores/Projeto02_ConstrutorPersonalizado/Program.cs
@@ -0,0 +1,9 @@
+﻿using Projeto02_ConstrutorPersonalizado.Class;
+
+//Colaborador colaborador = new Colaborador("Sofia","Médica",25000);
+
+Colaborador colaborador = new Colaborador();
+
+Console.WriteLine(colaborador.DadosColaborador());
+
+Console.ReadKey();
