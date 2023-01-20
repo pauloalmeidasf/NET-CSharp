2e2722abf5e2045038ec5c855522d3c2cb53a75d
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-19 21:59:23 -0300

    Commit Projeto02_Exercicio Capítulo 06

---
+++ Cap_06/Interfaces/Projeto02_Exercicio/Interfaces/ILog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02_Exercicio.Interfaces
+{
+    /*
+     * Interfaces
+     * 
+     * Permitem desenvolver sistemas com baixo acoplamento e mais flexível a mudanças. Se houver alterações
+     * será necessário apenas mudar a implementação
+     * 
+     * Ao usar interfaces, o desenvolvedor não precisa se preocupar como ela será implementada
+     * 
+     * Permite que o código se torne mais flexível, possibilitando trocar a implentação de um componente por outro
+     * 
+     * O código gerado é mais fácil de manter estender, visto que exite uma separação de responsabilidades entre
+     * os componentes do sistema e as regras de negócio
+     * 
+     * Facilita a realização de testes unitários
+     * 
+     */
+
+    internal interface ILog
+    {
+
+        void RegistroLog(string mensagem);
+
+    }
+}
