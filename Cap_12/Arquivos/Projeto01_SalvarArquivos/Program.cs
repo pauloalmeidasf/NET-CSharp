7ad44446f03b2d8e230712ec83e234646728f5fd
Paulo <paulo.almeidasantosferreira@gmail.com>
2023-01-24 22:54:16 -0300

    Commit Capítulo 12

---
+++ Cap_12/Arquivos/Projeto01_SalvarArquivos/Program.cs
@@ -0,0 +1,46 @@
+﻿//@: ignora caracteres de quebra de linha
+using Projeto01_SalvarArquivos.Class;
+
+string diretorioArquivo = @"c:\curso-csharp\aula-arquivos\";
+
+string arquivoTxt = @"c:\curso-csharp\aula-arquivos\clima.txt";
+
+string arquivoJson = @"c:\curso-csharp\aula-arquivos\clima.json";
+
+//Salvar arquivos
+
+List<Clima> listaClima = new List<Clima>
+{
+
+    new Clima(){Data = new DateTime(2023,01,22).Date, Cidade = "SAO PAULO", Temperatura = 25},
+    new Clima(){Data = new DateTime(2023,01,22).Date, Cidade = "CAMPINAS", Temperatura = 26},
+    new Clima(){Data = new DateTime(2023,01,22).Date, Cidade = "GUARULHOS", Temperatura = 27},
+    new Clima(){Data = new DateTime(2023,01,22).Date, Cidade = "UBERLANDIA", Temperatura = 30},
+
+};
+
+if(Directory.Exists(diretorioArquivo) == false)
+{
+
+    Directory.CreateDirectory(diretorioArquivo);
+
+}
+
+try
+{
+
+    Clima.SalvarTxt(arquivoTxt, listaClima);
+
+    Clima.SalvarJson(arquivoJson, listaClima);
+
+    Console.WriteLine("Arquivos salvos com sucesso.");
+
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e.Message);
+
+}
+
+Console.ReadKey();
---
+++ Cap_12/Arquivos/Projeto02_LerArquivos/Program.cs
@@ -0,0 +1,37 @@
+﻿using Projeto02_LerArquivos.Class;
+
+string arquivoTxt = @"C:\curso-csharp\aula-arquivos\clima.txt";
+
+string arquivoJson = @"C:\curso-csharp\aula-arquivos\clima.json";
+
+try
+{
+
+	string texto = Clima.ObtemDadosTxt(arquivoTxt);
+
+	Console.WriteLine("Dados lidos do arquivo txt");
+
+	Console.WriteLine();
+
+	Console.WriteLine(texto);
+
+	Console.WriteLine();
+	Console.WriteLine();
+
+	Console.WriteLine("Dados lidos do arquivo json");
+
+	Console.WriteLine();
+
+    Clima.OtemDadosJson(arquivoJson);
+
+}
+catch (Exception e)
+{
+
+	Console.WriteLine(e.Message);
+
+}
+
+
+
+Console.ReadKey();
